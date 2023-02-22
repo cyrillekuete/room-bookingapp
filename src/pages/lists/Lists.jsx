d1389312ba3a57830981330e69d8430186992fbd
Kuete Cyrille <cyrillekuete@outlook.com>
2023-02-22 16:37:52 +0100

    Property lists added

--- src/pages/lists/Lists.jsx
+++ src/pages/lists/Lists.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
 
 const Lists = () => {
   return (
-    <div>Lists</div>
-  )
-}
+    <div>
+      <Navbar />
+      <Header type="list" />
+    </div>
+  );
+};
 
-export default Lists
+export default Lists;
